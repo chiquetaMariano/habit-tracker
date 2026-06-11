4002cc1b00499e70831e96a39fca0ceb251be549
chiquetaMariano <mariano_cr90@hotmail.com>
2026-06-11 03:02:14 +0000

    fix navbar color

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,9 +5,12 @@ import { useEffect } from "react";
 import { Platform } from "react-native";
 import "react-native-reanimated";
 
+const APP_BACKGROUND_COLOR = "#0B1118";
+
 export default function RootLayout() {
   useEffect(() => {
     if (Platform.OS === "android") {
+      NavigationBar.setBackgroundColorAsync(APP_BACKGROUND_COLOR);
       NavigationBar.setButtonStyleAsync("light");
       NavigationBar.setVisibilityAsync("visible");
     }
